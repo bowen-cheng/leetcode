4a9a231a91f23b8dbbdee2ab8b1cd44644f5cb8b
Bowen Cheng <blueicycky@gmail.com>
2020-01-10 21:42:37 -0500

    Array intro 1 rename file

--- intro/arrays101/introduction/maxconsecutiveones/MaxConsecutiveOnes.java
+++ intro/arrays101/introduction/maxconsecutiveones/MaxConsecutiveOnes.java
@@ -9,7 +9,7 @@ package arrays101.introduction.maxconsecutiveones;
  * <p>The input array will only contain 0 and 1. The length of input array is a positive integer and
  * will not exceed 10,000
  */
-public class Solution {
+public class MaxConsecutiveOnes {
 
   public int findMaxConsecutiveOnes(int[] nums) {
 
